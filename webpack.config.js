84f637081b1d9b939fd59348e179b9b978948c49
zhao943018529 <943018529@qq.com>
2017-12-03 10:22:25 +0800

    initialize project structure

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin= require("html-webpack-plugin");
+const webpack = require('webpack');
+
+module.exports={
+	context: __dirname,
+	devtool:'source-map',
+	entry:{
+		app:'./src/index'
+	},
+	output:{
+		publicPath:'/',
+		path:path.join(__dirname+'/dist'),
+		filename:'js/[name].[hash].js',
+		chunkFilename:'js/[name].[chunkhash].js'
+	},
+	module:{
+		rules:[
+			{
+				test:/\.js$/,
+				exclude:/node_modules/,
+				use:{
+					loader:'babel-loader',
+					query:{
+						presets:[['env',{ "modules": false }],'react'],
+						plugins: ['transform-runtime']
+					}
+				}
+			},
+		{
+			test: /\.(sass|scss|less)$/,
+			use: [{
+				loader: "style-loader"
+			}, {
+				loader: "css-loader"
+			}, {
+				loader:'postcss-loader',
+				options: {
+					plugins: function() {
+						return [
+							require('precss'),
+							require('autoprefixer')
+						];
+					}
+				}
+			},{
+				loader: "sass-loader"
+			}]
+		}
+		]
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'commons',
+			filename: 'commons.js'
+		}),
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, 'src/index.html'),
+			hash: false,
+			filename: './index.html',
+			inject: 'body'
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.ProvidePlugin({
+			$:'jquery',
+			jQuery:'jquery'
+		})
+	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		port: 3000,
+		inline: true,
+		hot: true,
+		publicPath:'dist',
+		historyApiFallback: true
+	}
+
+}
